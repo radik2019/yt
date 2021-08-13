4d5cf2a831f31e480b8c520d755999f9de3932af
radik <codefacs@gmail.com>
2021-08-14 01:07:44 +0200

    non si converte piu, si scarica direttamente in formato mp3

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,5 +1,4 @@
 import pytube
-from moviepy.editor import VideoFileClip
 import os
 
 
@@ -10,41 +9,31 @@ else:
     os.mkdir("music")
     os.chdir("music")
 
-def convert_to_mp3(filename):
-    clip = VideoFileClip(filename)
-    print("converting..")
-    clip.audio.write_audiofile(filename[:-4] + ".mp3")
-    clip.close()
-    os.remove(filename)
-    print("converted!")
 
 def download_video(url):
 
     video = pytube.YouTube(url)
     titl = video.title
-    stream = video.streams.get_by_itag(22)
+    # stream = video.streams.get_by_itag(22)
+    stream  = video.streams.get_audio_only()
+    stream.subtype='mp3'
+    print(dir(stream))
     print(f"downloading '{titl}' ...")
     stream.download()
     print("file scaricato")
     return stream.default_filename
 
 def download_playlist(url):
-    # video = pytube.Youtube(url)
+
     playlist = pytube.Playlist(url)
     for link in playlist:
         video_file = download_video(link)
-        convert_to_mp3(video_file)
-    # return a list of links
     print(playlist.video_urls)
 
 
 if __name__ == "__main__":
-    flag = ''
-    while flag.lower().strip() != 'stop':
-        flag = input("inserisci link playlist per scaricare o 'stop' per anullare: ")
-
-
-    # download_playlist("https://www.youtube.com/watch?v=x1ZtIVuHph0&list=PLeTFVtsO70LZ5yYIvPimBej87Xcr14xdJ")
-
-# nm = download_video('https://www.youtube.com/watch?v=7MxzUaJowQc&list=PLQahUgl6RKL0lmixzOcl7KfStbc_SatOT&index=4')
-# convert_to_mp3(nm)
+    link = ''
+    while link.lower().strip() != 'stop':
+        link = input("inserisci link playlist per scaricare o 'stop' per anullare: ")
+        if link != "stop":
+            download_playlist(link)
